f5768e0ec6673226a1608cd14be9da282f63ec72
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-05-19 02:09:47 +0600

    Combination

--- Dynamic Programming/ncr.cpp
+++ Dynamic Programming/ncr.cpp
@@ -3,16 +3,14 @@ using namespace std;
 typedef long long ll;
 
 int ncr(int n, int r){
-	if(j>i)
+	if(r>n)
 		return 0;
-	ll arr[n+1][r+1];
-	for(int i=0; i<=n; i++)
-		arr[i][0] = 1;
-	for(int i=0; i<=r; i++)
-		arr[0][i] = 1;
+	ll arr[n+1][r+1] = {{0}};
     for(int i=1; i<=n; i++){
     	for(int j=1; j<=i && j<=r; j++){
-    		if(i==j)
+    		if(j==1)
+    			arr[i][j] = i;
+    		else if(i==j)
     			arr[i][j] = 1;
     		else
     			arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
